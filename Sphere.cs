84a491215c5c53a412c5e46207a3f22bf017f871
bobbycarver <bobbycarver38@gmail.com>
2017-03-14 08:43:53 +0000

    Add files via upload

---
+++ Sphere.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Name: Bobby Carver 
+ * Student number : 13170923
+ *  Birmingham City University
+ *  File: Sphere.CS V1.0
+ * Created on : 27/01/2015
+ * * */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1_ASD
+{
+    class Sphere
+    {
+        private double Radius; // defining radius 
+        private string  Material; // defining material 
+
+        // constraints 
+        const double PI = 3.14159; // sets PI 
+        const double DensityCopper = 8930 ;
+        const double DensityNickel = 8900;
+        const double DensityGold = 19300;
+        const double DensityAluminium = 27100;
+        const double DensityBronze = 8500;
+        const double DensitySteel = 7930;
+
+            
+   
+
+        public Sphere(string mat) // construct for 1 parameter
+        {
+            this.Material = mat;
+  
+
+         
+
+            switch (this.Material) // switch the radius based upon the material specified 
+            {
+                case "COPPER": // if the material is copper 
+                    this.Radius = 1.3;
+                    break;
+                case "NICKEL": // if the material is NICKEL
+                    this.Radius = 1.0;
+                    break;
+                case "GOLD": // if the material is GOLD
+                   this.Radius = 2.4 ;
+                    break; // break the case 
+                default: // if the string doesnt match run the default 
+                   this.Radius = 1.0;
+                    break; // break the case 
+            }
+
+      
+        }// end of con(1)
+
+        public Sphere(string mat, double rad) // construct for 2 parameters
+        {
+            this.Material = mat;
+            this.Radius = rad;
+        } // end of con(2)
+
+        public override string ToString() // to string method 
+        {
+            string message = "\n The material type selcted was " +this.Material + " And the radius selected is " + this.Radius; // to string message
+
+
+            return message; // returns message 
+
+
+        } // end of to string 
+
+        public double CalculateSurfaceArea() // calc surface area 
+        {
+            return (4 * PI) * (this.Radius * this.Radius);
+        } // end of surface area
+
+        public double CalculateVolume() // calc volume 
+        {
+            return (4 * PI) * (this.Radius * this.Radius * this.Radius) / 3;
+        } // end of calc volume
+
+        public double GetRadius() // get radius 
+        {
+            return this.Radius;
+        } // end of get radius 
+
+        public string GetMaterial() // get material 
+        {
+            return this.Material;
+        } // end of get material 
+
+        public double CalculateWeight() // calculate weight 
+        {
+            double weight; // defines weight 
+            switch (this.Material) // switch the answer based upon the material 
+            {
+                case "COPPER": // if the material is copper 
+                    weight = this.CalculateVolume() * DensityCopper; // do this calculation
+                    break;
+                case "NICKEL": // if the material is NICKEL
+                    weight = this.CalculateVolume() * DensityNickel; // do this calculation
+                    break;
+                case "GOLD": // if the material is GOLD
+                    weight = this.CalculateVolume() * DensityGold; // do this calculation
+                    break;
+                case "ALUMINIUM": // if the material is aluminium 
+                    weight = this.CalculateVolume() * DensityAluminium;
+                    break;
+                case "BRONZE": // if the material is bronze 
+                     weight = this.CalculateVolume() * DensityBronze;
+                     break;
+                case "STEEL": // if the material is steel 
+                     weight = this.CalculateVolume() * DensitySteel;
+                    break; // break the case 
+                default: // if the string doesnt match run the default 
+                    weight = 0.0; // default weight is 0
+                    break; // break the case 
+
+            }
+            return weight; // returns weight 
+
+        }
+
+    }
+    
+}
---
+++ Tester.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * Name: Bobby Carver 
+ * Student number : 13170923
+ *  Birmingham City University
+ *  File: Tester.CS V1.0
+ * Created on : 27/01/2015
+ * * */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1_ASD
+{
+    class Tester
+    {
+        static void Pause(string s)
+        {
+            Console.Write(s);
+            Console.Read();
+        }
+        public void Run()
+        {
+            Sphere S1; // sets the  Sphere to S1
+            int choice = 0; // users choice set to 0 
+            int flag = 0; // flag for loop set to 0 
+
+            
+            string material; // material 
+            double radius; // radius 
+
+
+            // introduction messages.
+            string intro = "\n Program to calculate properties of a Sphere of various materials";
+            intro += "\n\n Author: Bobby Carver  (c) 2015";
+            intro += "\n Student Number 13170923";
+            Console.Title = "Sphere of material application";
+            Console.Clear(); // clear screen 
+            Console.WriteLine(intro); // writes message to screen 
+
+            while (flag == 0) // while the flag is set at 0 run the following code 
+            {
+                do // second loop within a loop based on users actions
+                {
+                    //prints user options onto the screen
+                    Console.WriteLine("\n Choose from : ");
+                    Console.WriteLine("\nt1. Create Sphere of unit dimensions, with specified material");
+                    Console.WriteLine("\nt2. Create Sphere of specified dimensions and material");
+                    Console.Write("\n 1 or 2 ? => ");
+                    choice = int.Parse(Console.ReadLine());
+
+                }
+                while ((choice != 1) && (choice != 2));
+                // END OF CHOICE WHILE LOOP(2)
+
+                if (choice == 1)
+                {
+                    Console.Write("\n Enter material type => ");
+                    material = Console.ReadLine();
+                    material = material.ToUpper();
+                    S1 = new Sphere(material); // creates cone using 1 parameter as user selected option 1 
+                }
+                else
+                {
+                    Console.Write("\n Enter material type => ");
+                    material = Console.ReadLine();
+                    material = material.ToUpper();
+                    Console.Write("\n Enter a radius => ");
+                    radius = double.Parse(Console.ReadLine());
+
+                   if(radius >= 1.0) // CHECKS TO SEE IF THE RADIUS IS VALID BY BEEING GREATER THAN 1 OR = TO 1 
+                   {
+
+
+                    S1 = new Sphere(material, radius); // RUNS FINE IF VALID RADIUS IS ENTERED 
+                   }
+                   else // IF NO VALID RADIUS MEETS THE CONDITION SETS THE RADIUS TO 1 AS DEFAULT.  
+                   {
+
+                       Console.WriteLine("you have entered a radius of 0 or a negative radius, the radius has been set to 1.0 as a default.");
+                    radius = 1.0;
+                    S1 = new Sphere(material, radius); // CREATES SPHERE AS RADIUS AS DEFAULT 1 
+                   }
+                } // end of if statement with users choice 
+
+
+
+
+
+                Console.WriteLine("\n The Sphere details are as follows:");
+
+                Console.WriteLine(S1.ToString()); // calls and writes the two string method
+
+                Console.WriteLine("\n The Surface of the Sphere is " + S1.CalculateSurfaceArea()); // uses method from sphere and prints to screen 
+                Console.WriteLine("\n The volume of the Sphere is " + S1.CalculateVolume()); // uses method from sphere and prints to screen 
+                Console.WriteLine("\n The weight of the Sphere is  " + S1.CalculateWeight()); // uses method from sphere and prints to screen  
+                Console.WriteLine("Would you like to run the program again to test another Sphere? Y/N"); // askes for user input 
+                String ans; // new veriable for user answer 
+                    ans = Console.ReadLine();
+                ans = ans.ToUpper(); // converts to upper 
+                if(ans == "Y") // if the answer is Y then keep the flag at 0 and clear the screen so the user can do another spher e
+                {
+                    flag = 0;
+                    Console.Clear();
+                }
+                else  // if the user says no 
+                {
+                    flag = 1; // makes the loop exit 
+                }
+
+            } // end of while loop 
+
+        }
+        static void Main()
+        {
+            Tester t = new Tester();
+            t.Run();
+            Pause("\nPress any key to exit ...");
+        }
+    }
+}
